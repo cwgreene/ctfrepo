536c81fe7d5c4512e3c2113d8e2e25f0e8257eb0
Chris Greene <archgoon@gmail.com>
2020-05-24 10:53:45 -0700

    fix time.

--- track-a-sat/groundstation.py
+++ track-a-sat/groundstation.py
@@ -2,6 +2,8 @@ from skyfield.api import Topos, load, utc
 
 import datetime
 
+planets = load('de421.bsp')
+
 stations = {station.name:station for station in load.tle_file("./active.txt")}
 station = stations["PERUSAT 1"]
 ts = load.timescale()
@@ -11,17 +13,16 @@ lon = 85.18
 groundstation = Topos(lat, lon)
 
 def az_degrees_to_pwm(degrees):
-    print(degrees)
     return int(2457 + (7372 - 2457)*(degrees/360))
 
 def elevation_degrees_to_pwm(degrees):
     print(degrees)
-    return int(2457 + (7372 - 2457)*(degrees/180))
+    return int(2457 + (7372 - 2457)*((degrees)/180))
 
 starttime = 1586789933.820023
 for i in range(720):
     time = starttime + i
-    t = ts.utc(datetime.datetime.fromtimestamp(time).replace(tzinfo=utc))
+    t = ts.utc(datetime.datetime.utcfromtimestamp(time).replace(tzinfo=utc))
     distance = station - groundstation
-    alt, az, distance = distance.at(t).altaz()
+    alt, az, d = distance.at(t).altaz()
     print(time,elevation_degrees_to_pwm(alt.degrees),az_degrees_to_pwm(az.degrees))
